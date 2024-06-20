df2c44ecdff7fa6c8326fff81f090b8979a52ac7
Daniele <daniele.capa@gmail.com>
2024-06-20 10:49:53 +0200

    Added finished homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,28 +1,66 @@
 import React from 'react';
-import Logo from '../assets/imgs/Logo.jpg'; // Adjust the path as per your project structure
+import { Link } from 'react-router-dom';
+import Logo from '../assets/imgs/Logo.jpg';
+import Buttons from '../assets/imgs/Buttons.jpg';
 
 function HomePage() {
+  const buttonStyle = {
+    backgroundImage: `url(${Buttons})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  };
+
   return (
-    <div className="bg-black h-screen flex justify-center items-center">
-      <div className="flex flex-col items-center">
+    <div className="bg-black min-h-screen flex flex-col justify-between">
+      {/* Main content area */}
+      <div className="flex flex-col items-center justify-center flex-grow">
         {/* Upper div with background image */}
-        <div className="bg-red-500 h-48 w-96 mb-2" style={{
+        <div className="w-full max-w-lg h-64 mb-4" style={{
             backgroundImage: `url(${Logo})`,
-            backgroundSize: 'cover', // Adjusted to cover
+            backgroundSize: 'contain',
+            backgroundRepeat: 'no-repeat',
             backgroundPosition: 'center',
           }}>
         </div>
         
         {/* Lower div */}
-        <div className="flex w-2/4 h-1/2">
-          {/* Left sub-div */}
-          <div className="bg-blue-500 w-1/3 h-full mr-2">b</div>
-          {/* Middle sub-div */}
-          <div className="bg-green-500 w-1/3 h-full mr-2">c</div>
-          {/* Right sub-div */}
-          <div className="bg-yellow-500 w-1/3 h-full">d</div>
+        <div className="flex justify-around w-full max-w-4xl h-16">
+          {/* Sub-divs */}
+          <Link to="/about"
+            className="relative bg-gray-800 w-1/3 flex justify-center items-end mx-1 hover:bg-gray-700 transition duration-300"
+            style={buttonStyle}
+            onMouseEnter={(e) => e.target.style.backgroundBlendMode = 'multiply'}
+            onMouseLeave={(e) => e.target.style.backgroundBlendMode = 'normal'}
+            onClick={(e) => e.target.style.filter = 'brightness(90%)'}
+          >
+            <span className="text-black tracking-widest">ABOUT ME</span>
+          </Link>
+          <Link to="/projects"
+            className="relative bg-gray-800 w-1/3 flex justify-center items-end mx-1 hover:bg-gray-700 transition duration-300"
+            style={buttonStyle}
+            onMouseEnter={(e) => e.target.style.backgroundBlendMode = 'multiply'}
+            onMouseLeave={(e) => e.target.style.backgroundBlendMode = 'normal'}
+            onClick={(e) => e.target.style.filter = 'brightness(90%)'}
+          >
+            <span className="text-black tracking-widest">PROJECTS</span>
+          </Link>
+          <Link to="/contact"
+            className="relative bg-gray-800 w-1/3 flex justify-center items-end mx-1 hover:bg-gray-700 transition duration-300"
+            style={buttonStyle}
+            onMouseEnter={(e) => e.target.style.backgroundBlendMode = 'multiply'}
+            onMouseLeave={(e) => e.target.style.backgroundBlendMode = 'normal'}
+            onClick={(e) => e.target.style.filter = 'brightness(90%)'}
+          >
+            <span className="text-black tracking-widest">CONTACTS</span>
+          </Link>
         </div>
       </div>
+      
+      {/* Footer */}
+      <footer className="bg-black text-gray-500 text-right py-4 pr-4">
+        <p>&copy; 2024 Daniele Capaccetti. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    letterSpacing: {
+      tightest: '-.075em',
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      wider: '.05em',
+      widest: '.1em',
+    }
   },
   plugins: [],
 }
